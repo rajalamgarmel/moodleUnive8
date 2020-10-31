835aa98d5488f962d655819ec6fa6de6fe48fb4c
rajalamgarmel <lamgarmel.raja@gmail.com>
2020-10-31 18:53:30 +0100

    configurer la route home ajouter la base de notre template base.html ajouter la page web de notre accueil

---
+++ app/home/views.py
@@ -0,0 +1,16 @@
+# app/home/views.py
+from flask import abort, render_template
+from flask_login import current_user, login_required
+
+from . import home
+
+
+@home.route('/')
+def homepage():
+    """
+    Render the homepage template on the / route
+    """
+    return render_template('home/index.html', title="Welcome")
+
+
+
